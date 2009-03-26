6cb69e810ad92714875ed37202cedd93e2efd776
Justin Azoff <justin@bouncybouncy.net>
2009-03-26 11:31:45 -0400

    added node tests

---
+++ tests/test_nodes.py
@@ -0,0 +1,17 @@
+from netdisco import db
+import sqlalchemy
+import sys
+
+def test_start():
+    print "Testing %s" % (sys.version)
+    print "on SA ver: %s" % sqlalchemy.__version__
+
+
+IP = '10.1.2.2'
+MAC = '0003.bacd.b521'
+
+
+def test_active_nodes():
+    #for now just make sure nothing is broken
+    ips = db.Node_IP.get_active_ips()
+    assert len(ips) > 1000
