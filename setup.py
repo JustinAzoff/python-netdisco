20482dc63725b2f2bd354fa63e926520bc949014
Justin Azoff <justin@bouncybouncy.net>
2011-07-12 10:17:07 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from glob import glob
 
 setup(name='netdisco',
-    version='1.0',
+    version='1.1',
     scripts=glob('scripts/*'),
     packages = ['netdisco'],
     install_requires=[
